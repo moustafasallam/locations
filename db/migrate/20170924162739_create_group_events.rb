e19955bba8fa6033b933d424e75a91d31a093bff
Moustafa Sallam <moustafasallam@hotmail.com>
2017-11-16 03:24:42 +0200

    build the location tables

--- db/migrate/20170924162739_create_group_events.rb
+++ db/migrate/20170924162739_create_group_events.rb
@@ -1,19 +1,24 @@
 class CreateGroupEvents < ActiveRecord::Migration
   def change
     create_table :locations do |t|
-      t.string :name, default: ""
-      t.text :description
-      t.string :state, default:  "draft"
-      t.date :start_date
-      t.date :end_date
-      t.string :location, default: ""
-      t.float :latitude
-      t.float :longitude
-      t.datetime :deleted_at
-
+      t.string  :city_name, default: ""
+      t.integer :city_id, default: 0
+      t.string  :country_code, default: ""
+      t.integer :sunrise, default: 0
+      t.integer :sunset, default: 0
+      t.float :wind_speed, default: 0.0
+      t.integer :wind_degree, default: 0
+      t.float :temperature, default: 0.0
+      t.float :temp_min, default: 0.0
+      t.float :temp_max, default: 0.0
+      t.integer :humidity, default: 0
+      t.string :weather_title, default: ""
+      t.string :weather_description, default: ""
       t.timestamps null: false
     end
 
-    add_index :group_events, :deleted_at
+    add_index :locations, :city_name
+    add_index :locations, :city_id, unique: true
+    add_index :locations, :country_code
   end
 end
